ef39fbcd8e4858267147e13b0620495811d49902
jchck <hey@justinchick.com>
2016-07-18 15:53:48 -0400

    turn off admin pages

--- setup.php
+++ setup.php
@@ -33,7 +33,32 @@ function setup(){
 	if ( current_user_can( 'manage_options' ) ) {
 		show_admin_bar( false );
 	}
+
+	/*
+		Turn off the ACF admin page if < admin
+		@link https://www.advancedcustomfields.com/resources/how-to-hide-acf-menu-from-clients/
+	*/
+	
+	if ( ! current_user_can( 'manage_options' ) ) {
+		add_filter('acf/settings/show_admin', '__return_false');
+	}
 	
 }
 
-add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
+add_action( 'after_setup_theme', __NAMESPACE__ . '\\setup' );
+
+
+
+function remove_pages(){
+
+	/* 
+		Turn off Plugins if < admin
+		https://codex.wordpress.org/Function_Reference/remove_menu_page
+	*/
+	
+	if ( ! current_user_can( 'manage_options' ) ) {
+		remove_menu_page( 'plugins.php' );
+	}
+}
+
+add_action( 'admin_menu', __NAMESPACE__ . '\\remove_pages' );
