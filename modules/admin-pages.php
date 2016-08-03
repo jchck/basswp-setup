69c9f887a18607a8de804654f28d3a0acd10fff5
jchck <hey@justinchick.com>
2016-08-03 18:56:20 -0400

    clean up dashboard widgets

--- modules/admin-pages.php
+++ modules/admin-pages.php
@@ -3,6 +3,11 @@
 namespace basswp\setup\pages;
 
 function remove_options(){
+
+	/*
+		Turn off options pages
+	*/
+
 	/* Posts */
 	//remove_menu_page( 'edit.php' );
 	/* Categories */
@@ -41,3 +46,23 @@ function admin(){
 }
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\admin' );
+
+function widgets(){
+
+	/*
+		Turn off dashboard widgets
+	*/
+
+	global $wp_meta_boxes;
+
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+	//unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+}
+
+add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\\widgets' );
