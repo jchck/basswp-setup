e3614b989bb57807e1dd9369956a15e5d6d9f30c
jchck <hey@justinchick.com>
2016-08-03 20:02:45 -0400

    remove items from admin bar

--- modules/admin.php
+++ modules/admin.php
@@ -24,8 +24,8 @@ function remove_options(){
 	/* Tools */
 	remove_menu_page( 'tools.php' );
 }
-
 add_action( 'admin_menu', __NAMESPACE__ . '\\remove_options' );
+
 
 function admin(){
 
@@ -44,8 +44,26 @@ function admin(){
 	add_filter('acf/settings/show_admin', '__return_false');
 	
 }
-
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\admin' );
+
+function admin_bar(){
+
+	/*
+		Remove items from admin bar
+	*/
+
+	global $wp_admin_bar;
+
+	$wp_admin_bar->remove_menu('wp-logo');
+	$wp_admin_bar->remove_menu('about');
+	$wp_admin_bar->remove_menu('wporg');
+	$wp_admin_bar->remove_menu('documentation');
+	$wp_admin_bar->remove_menu('support-forums');
+	$wp_admin_bar->remove_menu('feedback');
+	$wp_admin_bar->remove_menu('view-site');
+}
+add_action( 'wp_before_admin_bar_render', __NAMESPACE__ . '\\admin_bar' );
+
 
 function widgets(){
 
@@ -64,5 +82,6 @@ function widgets(){
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
 }
+
 
 add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\\widgets' );
